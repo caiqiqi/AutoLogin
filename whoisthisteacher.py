b54dda28da553016ed86402634b239c0f7644b19
CaiQiqi <564933437@qq.com>
2016-05-05 21:27:52 +0800

    #1 temperorily done

--- whoisthisteacher.py
+++ whoisthisteacher.py
@@ -2,6 +2,7 @@
 # this tool is used for quering teachers in CQUPT, whose data is from
 # its official website  http://www.cqupt.edu.cn
 
+import sys
 import requests
 import lxml.html
 
@@ -43,20 +44,51 @@ makesi_url_1        = "http://marx.cqupt.edu.cn/home?method=childPhotoShow&categ
 makesi_url_2        = "http://marx.cqupt.edu.cn/home?method=childPhotoShow&category=%E6%95%99%E6%8E%88"
 
 
+main_xpath  = '//div[@class="_s1 select"]/ul/li/a'
 ruanjian_xpath = '//body/form[@id="aspnetForm"]/div[@id="main"]/div[@class="content"]/div[@class="list_right mt20"]/div[@class="news_list mt15"]/div/div/div[@id="ctl00_ContentPlaceHolder1_UpdatePanel1"]/ul/li/span[@class="text"]/a'
-waiguoyu_xpath = '//body[@class="channle"]/table/tbody/tr/td[@class="news_list"]/table[@class="box"]/tbody/tr/td/li/a'
+waiguoyu_xpath = '//li/a[@target="_blank"]'
+lixueyuan_xpath = '//font[@style="font-size:12px;line-height:30px;"]/a[@target="_blank"]'
+jingguan_xpath = '//div[@id="container"]/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/table[2]/tbody/tr[2]/td/table/tbody/tr/td/a'
 
 
 
-def get_teacher_name(college_url, xpath_expression):
+def get_teacher_name(college_url, xpath_expression, tp):
     resp = requests.get(college_url)
     doc = lxml.html.document_fromstring(resp.content)
-    html_uls = doc.xpath(xpath_expression)
-    for html_ul in html_uls:
-        print html_ul.text
 
+    #软件学院 DONE
+    if tp == 'ruanjian':
+        html_uls = doc.xpath(xpath_expression)     # list类型
+        print "得到的list长度为：", str(len(html_uls))
+        for html_ul in html_uls:
+            print html_ul.text
+    #外国语学院
+    elif tp == 'waiguoyu':
+        html_uls = doc.xpath(xpath_expression)
+        print len(html_uls)
+        html_uls = doc.xpath(xpath_expression)
+    	for html_ul in html_uls:
+    		print html_ul.text
+    #理学院 DONE
+    elif tp == 'lixueyuan':
+    	html_uls = doc.xpath(xpath_expression)
+    	for html_ul in html_uls:
+    		print html_ul.text
+    #主页 DONE
+    elif tp == 'main':
+    	html_uls = doc.xpath(xpath_expression)
+    	for html_ul in html_uls:
+    		print html_ul.text
+
+#主页
+#get_teacher_name(main_url, main_xpath, 'main')
 #软件学院 //TODO: 对js进行跳转到下一页的情况进行处理
-get_teacher_name(ruanjian_url_0, ruanjian_xpath)
+get_teacher_name(ruanjian_url_0, ruanjian_xpath, 'ruanjian')
 #外国语学院
-#get_teacher_name(waiguoyu_url_0, waiguoyu_xpath)
-
+#get_teacher_name(waiguoyu_url_0, waiguoyu_xpath, 'waiguoyu')
+#理学院
+#get_teacher_name(lixueyuan_url_0, lixueyuan_xpath , 'lixueyuan')
+#经管学院
+#get_teacher_name(jingguan_url_1, jingguan_xpath)
+#理学院
+#get_teacher_name(lixueyuan_url_0, , 'lixueyuan')
