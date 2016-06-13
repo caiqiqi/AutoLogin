2e0d2c1f3ad530d7bb0f96be293b68a2716e2e7a
CaiQiqi <564933437@qq.com>
2016-06-14 02:53:31 +0800

    except the order of post form is not correct

--- parser.py
+++ parser.py
@@ -6,36 +6,52 @@ import re
 # 用于显示表格的
 # from prettytable import PrettyTable
 
-TAG_INPUT = 'input'
-ID_VIEWSTATE = "__VIEWSTATE"
-ID_EVENTVALIDATION = "__EVENTVALIDATION"
+
 
 # 用于找到sessionId的
 _pattern_1 = "Set-Cookie"
 _pattern_2 = ";"
 
 
-def parse_to_post_params(html_doc):
+def parse_to_post_params(html_str):
     '''
-    从 html_doc中解析出两个参数
-    :param html_doc: 待解析的html页面
-    :return: 包含两个已解析的参数的字典
+    从 html_str中解析出三个参数
+    __VIEWSTATE,
+    __EVENTVALIDATION,
+    验证码带随机数字的url
     '''
-    # soup 就是BeautifulSoup处理格式化后的字符串
-    soup = BeautifulSoup(html_doc, "lxml")
-    # 找到所有`input`标签,find_all 函数返回的是一个序列
-    soup.find_all(TAG_INPUT)
-    # 得到VIEWSTATE的值
-    value_VIEWSTATE = soup.find(id= ID_VIEWSTATE)['value']
-    # 得到EVENTVALIDATION的值
-    value_EVENTVALIDATION= soup.find(id = ID_EVENTVALIDATION)['value']
-    dict_values = {}
-    dict_values['__VIEWSTATE'] = value_VIEWSTATE
-    dict_values['__EVENTVALIDATION'] = value_EVENTVALIDATION
-    print dict_values['__VIEWSTATE']
-    print dict_values['__EVENTVALIDATION']
 
-    return dict_values
+    TAG_INPUT = 'input'
+    ID_VIEWSTATE = "__VIEWSTATE"
+    ID_EVENTVALIDATION = "__EVENTVALIDATION"
+    ID_VALIDATECODE = "ctl00_contentParent_ValidateImage"
+
+    # soup 就是BeautifulSoup处理格式化后的字符串
+    soup = BeautifulSoup(html_str, "lxml")
+    # 找到所有`input`标签,find_all 函数返回的是一个序列
+    # soup.find_all(TAG_INPUT)
+    # 得到VIEWSTATE的值
+    value_VIEWSTATE         = soup.find(id= ID_VIEWSTATE)['value']
+    # 得到EVENTVALIDATION的值
+    value_EVENTVALIDATION   = soup.find(id = ID_EVENTVALIDATION)['value']
+    # 得到验证码带随机数字的url
+    value_validatecode_url  = soup.find(id = ID_VALIDATECODE)['src']
+
+    return value_VIEWSTATE, value_EVENTVALIDATION, value_validatecode_url
+
+
+def parse_validatecode_url(html_str):
+    '''
+    由于请求验证码的url中带有一个随机的9位数字，所以每次得从url_loging页面里找到这个验证码的url
+    '''
+#获取动态隐藏的参数，为post做准备
+def get_hiddenvalue(url):
+    request=urllib2.Request(url)
+    reponse=urllib2.urlopen(request)
+    resu=reponse.read()
+    VIEWSTATE =re.findall(r'<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)" />', resu,re.I)
+    EVENTVALIDATION =re.findall(r'input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*?)" />', resu,re.I)
+    return VIEWSTATE[0],EVENTVALIDATION[0]
 
 def parse_to_courses_selected(html_doc):
     '''
--- student-login-final.py
+++ student-login-final.py
@@ -18,6 +18,10 @@ from parser import parse_to_post_params
 # 带着这个header，用POST提交到Relogin.aspx(这时解析ini文件中的信息)
 # 测试是否登录成功
 
+# 开始就用一个session来维持整个回话，这样在之后请求中可以沿用之前的headers，
+# 而且可以往headers里添加已有的字段以覆盖之前的字段，这样connection才是『keep-alive』，而不是close
+s = requests.Session()
+
 CONFIG_FILE = 'config.ini'
 CONFIG_ITEM_INFO = 'info'
 
@@ -25,10 +29,10 @@ _username = ''
 _password = ''
 _result_captcha = ''
 
-_url_captcha = "http://gs.cqupt.edu.cn:8080/Public/ValidateCode.aspx"
-_file_captcha = "captcha.png"
-_url_loging = "http://gs.cqupt.edu.cn:8080/gstudent/loging.aspx?undefined"
-_url_relogin = "http://gs.cqupt.edu.cn:8080/gstudent/ReLogin.aspx?ReturnUrl=/gstudent/loging.aspx?undefined"
+_file_captcha     = "captcha.png"
+_url_captcha      = "http://gs.cqupt.edu.cn:8080/Public/ValidateCode.aspx"
+_url_loging       = "http://gs.cqupt.edu.cn:8080/gstudent/loging.aspx?undefined"
+_url_relogin      = "http://gs.cqupt.edu.cn:8080/gstudent/ReLogin.aspx?ReturnUrl=/gstudent/loging.aspx?undefined"
 _url_course_query = "http://gs.cqupt.edu.cn:8080/gstudent/Course/CourseSelQuery.aspx?EID=Ng!0IdeEcMBa4v7gTkZteOPL5Mjmu7TIBdO8k2iXxW479MCMokufJQ=="
 
 _cookie = "Cookie"
@@ -43,6 +47,16 @@ headers0 = {
     "Host": "gs.cqupt.edu.cn:8080",
     "Upgrade-Insecure-Requests": "1"
 }
+
+headers_image = {
+    "Host": "gs.cqupt.edu.cn:8080",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36",
+    "Accept": "image/webp,image/*,*/*;q=0.8",
+    "Referer": "http://gs.cqupt.edu.cn:8080/gstudent/ReLogin.aspx?ReturnUrl=/gstudent/loging.aspx?undefined",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.6,en;q=0.4"
+} 
 # 要POST给登录页面的
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0",
@@ -50,7 +64,6 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
     "Accept-Encoding": "gzip, deflate",
     "Cache-Control": "max-age=0",
-    "Cookie": _cookie_value_318_2055,
     "Connection": "keep-alive",
     "Host": "gs.cqupt.edu.cn:8080",
     "Origin": "http://gs.cqupt.edu.cn:8080",
@@ -83,11 +96,6 @@ headers2 = {
     "Origin": "http://gs.cqupt.edu.cn:8080"
 }
 
-headers_final = {
-    "Accept":     "*/*",
-    "Referer":    "http://gs.cqupt.edu.cn:8080/gstudent/Course/CourseSelQuery.aspx?EID=Ng!0IdeEcMBa4v7gTkZteOPL5Mjmu7TIBdO8k2iXxW479MCMokufJQ==",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
-}
 
 
 def _get_item_from_ini():
@@ -102,15 +110,13 @@ def _get_item_from_ini():
     global _password
     _username = cf.get(CONFIG_ITEM_INFO, 'username')
     _password = cf.get(CONFIG_ITEM_INFO, 'password')
-    # 不要将四个空格喝TAB键混用了
+    # 不要将四个空格和TAB键混用了
 
 def _save_img_from_url(imageUrl, filename):
     '''
     get the captcha by the url_captcha and save the image as 'captcha.png'
     '''
-    r = requests.get(imageUrl)
-    # with open(filename, 'w') as fp:
-    # shutil.copyfileobj(data, fp)
+    r = s.get(imageUrl, headers= headers_image)
     f = open(filename, 'wb')
     f.write(r.content)
     f.close()
@@ -128,7 +134,6 @@ def _is_file_empty(filepath):
 def _parse_img_to_txt(image_file):
     if not _is_file_empty(image_file):
         result = pytesseract.image_to_string(Image.open(image_file))
-        print result
         return result
     else:
         print "File size zero!"
@@ -142,19 +147,28 @@ def _parse_img_to_txt(image_file):
 # 从ini文件中解析出账号密码信息
 _get_item_from_ini()
 print(_username)
+
+
+# 先GET到`登录页面`
+_r0 = s.get(_url_loging, headers = headers0)
+# 解析GET到的页面中的两个关键元素
+# dict_post_params = parse_to_post_params(_r0.content)
+# print dict_post_params['__VIEWSTATE']
+# print dict_post_params['__EVENTVALIDATION']
+VIEWSTATE, EVENTVALIDATION, _url_captcha_tmp = parse_to_post_params(_r0.content)
+# 分割出从页面中得到的参数并将其连接到验证码的url上去
+_url_captcha = _url_captcha + "?" + _url_captcha_tmp.split('?')[1]
+print VIEWSTATE
+print EVENTVALIDATION
+print "待访问的带随机数字的验证码的url："
+print _url_captcha
+
 # 从向这个url发出请求,然后将得到的图片保存到本地
 _save_img_from_url(_url_captcha, _file_captcha)
 # 解析保存到本地的图片
 _result_captcha = _parse_img_to_txt(_file_captcha)
+print "验证码："
 print _result_captcha
-
-# 先GET到`登录页面`
-_r0 = requests.get(_url_relogin, headers = headers0)
-# 解析GET到的页面中的两个关键元素
-dict_post_params = parse_to_post_params(_r0.content)
-print dict_post_params['__VIEWSTATE']
-print dict_post_params['__EVENTVALIDATION']
-
 
 ### 注意：即便你已经登录了，你再开一个浏览器标签页，然后发出一个这样的请求之后，那你之前已经登录的那个标签页再刷新的时候也失去了登录状态，返回一个重新登录的页面让你重新登录。
 
@@ -167,15 +181,24 @@ print dict_post_params['__EVENTVALIDATION']
 _url_payload = {
     '__EVENTTARGET': 'ctl00$contentParent$btLogin',
     '__EVENTARGUMENT': '',
-    '__VIEWSTATE': dict_post_params['__VIEWSTATE'],
-    '__EVENTVALIDATION': dict_post_params['__EVENTVALIDATION'],
+    '__VIEWSTATE': VIEWSTATE,
+    '__EVENTVALIDATION': EVENTVALIDATION,
     'ctl00$contentParent$UserName': _username,
     'ctl00$contentParent$PassWord': _password,
     'ctl00$contentParent$ValidateCode': _result_captcha
 }
 
+'''
+__EVENTTARGET=ctl00%24contentParent%24btLogin&
+__EVENTARGUMENT=&
+__VIEWSTATE=%2FwEPDwUKMTA4ODc5NDc0OA9kFgJmD2QWAgIDD2QWAgIDD2QWAgILD2QWAmYPZBYCAgEPDxYCHghJbWFnZVVybAUrfi9QdWJsaWMvVmFsaWRhdGVDb2RlLmFzcHg%2FaW1hZ2U9MTkzMTkxNDc0OGRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBSFjdGwwMCRjb250ZW50UGFyZW50JFZhbGlkYXRlSW1hZ2U%3D&
+__EVENTVALIDATION=%2FwEdAAZ%2BE1NX%2B%2Fbbjbsz%2BB%2BD%2BrDncybtMhw0mn0LtKqAHeD%2F6LR%2FVkzxozH4tyiImdrtlAcUWWYub4JHktVQEGONTxqoRZzhTcnfFsWcwOVyhy6aT8GiwGHwM4Wl4obxma9ASls%3D&
+ctl00%24contentParent%24UserName=s150231003&
+ctl00%24contentParent%24PassWord=HBJMcqq2215746&
+ctl00%24contentParent%24ValidateCode=0607
+'''
 # _r = _session.post(_url_relogin, data=_url_payload, headers=headers)
-_r = requests.post(_url_loging, data= json.dumps(_url_payload), headers=headers)
+_r = s.post(_url_loging, data= _url_payload, headers=headers)
 print _r.url
 
 
@@ -186,5 +209,6 @@ print _r.url
 # 如何判断是非已经登陆成功
 
 
-_r2 = requests.get(_url_course_query, headers=headers2)
+_r2 = s.get(_url_course_query, headers=headers2)
 print _r2.url
+#print _r2.content
