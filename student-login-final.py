e8aeca22a7dd528ec62bad14c4cb7c10fa4dd945
CaiQiqi <564933437@qq.com>
2016-06-14 09:12:24 +0800

    added def main

--- student-login-final.py
+++ student-login-final.py
@@ -2,15 +2,13 @@
 # -*- coding:utf-8 -*-
 __author__ = 'caiqiqi'
 
-import requests
 import os
-import json
+
 # ConfigParser 用于从.ini文件中读取信息的
 import ConfigParser
-
+import requests
 import pytesseract
 from PIL import Image
-
 from bs4 import BeautifulSoup
 
 
@@ -31,6 +29,7 @@ _url_captcha      = "http://gs.cqupt.edu.cn:8080/Public/ValidateCode.aspx"
 _url_loging       = "http://gs.cqupt.edu.cn:8080/gstudent/loging.aspx?undefined"
 _url_relogin      = "http://gs.cqupt.edu.cn:8080/gstudent/ReLogin.aspx?ReturnUrl=/gstudent/loging.aspx?undefined"
 _url_course_query = "http://gs.cqupt.edu.cn:8080/gstudent/Course/CourseSelQuery.aspx?EID=Ng!0IdeEcMBa4v7gTkZteOPL5Mjmu7TIBdO8k2iXxW479MCMokufJQ=="
+_url_exam_info    = "http://gs.cqupt.edu.cn:8080/gstudent/Course/CourseTestInfo.aspx?EID=wlt4FUXSIZqP8ASNy5fY7O3mF9B6EclOO8VWx82rzmj1PxzhUTNKUg=="
 
 
 headers0 = {
@@ -71,7 +70,7 @@ headers_post = {
 headers_query = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.6,en;q=0.4",
     "Accept-Encoding": "gzip, deflate",
     "Upgrade-Insecure-Requests": "1",
     "Cache-Control": "max-age=0",
@@ -94,42 +93,30 @@ def _get_item_from_ini():
     global _password
     _username = cf.get(CONFIG_ITEM_INFO, 'username')
     _password = cf.get(CONFIG_ITEM_INFO, 'password')
-    # 不要将四个空格和TAB键混用了
 
-def _save_img_from_url(imageUrl, filename):
-    '''
-    get the captcha by the url_captcha and save the image as 'captcha.png'
-    '''
-    r = s.get(imageUrl, headers= headers_image)
+
+def save_img_to_file_and_get_result(imageUrl, filename):
+    r = requests.get(imageUrl)
     f = open(filename, 'wb')
     f.write(r.content)
     f.close()
 
-def _is_file_empty(filepath):
-    '''
-    判断文件内容是否为空
-    '''
-    file_size = os.stat(filepath).st_size
-    if 0 == file_size:
-        return True
-    else:
-        return False
-
-def _parse_img_to_txt(image_file):
-    if not _is_file_empty(image_file):
-        result = pytesseract.image_to_string(Image.open(image_file))
+    # 判断文件内容是否为空
+    file_size = os.stat(filename).st_size
+    if 0 != file_size:
+        # 将读到的图片转换成文本
+        result = pytesseract.image_to_string(Image.open(filename))
+        print result
         return result
     else:
-        print "File size zero!"
-        return ""
-
+        print "解析验证码错误！"
 
 def parse_to_post_params(html_str):
     '''
     从 html_str中解析出三个参数
-    __VIEWSTATE,
-    __EVENTVALIDATION,
-    验证码带随机数字的url
+    1. __VIEWSTATE,
+    2. __EVENTVALIDATION,
+    3. 验证码带随机数字的url
     '''
 
     ID_VIEWSTATE = "__VIEWSTATE"
@@ -138,8 +125,7 @@ def parse_to_post_params(html_str):
 
     # soup 就是BeautifulSoup处理格式化后的字符串
     soup = BeautifulSoup(html_str, "lxml")
-    # 找到所有`input`标签,find_all 函数返回的是一个序列
-    # soup.find_all(TAG_INPUT)
+    
     # 得到VIEWSTATE的值
     value_VIEWSTATE         = soup.find(id= ID_VIEWSTATE)['value']
     # 得到EVENTVALIDATION的值
@@ -150,42 +136,46 @@ def parse_to_post_params(html_str):
     return value_VIEWSTATE, value_EVENTVALIDATION, value_validatecode_url
 
 
+def main():
+    # 从ini文件中解析出账号密码信息
+    _get_item_from_ini()
+    print(_username)
 
-# 从ini文件中解析出账号密码信息
-_get_item_from_ini()
-print(_username)
 
+    # 先GET到`登录页面`
+    _r0 = s.get(_url_loging, headers = headers0)
+    VIEWSTATE, EVENTVALIDATION, _url_captcha_tmp = parse_to_post_params(_r0.content)
+    # 分割出从页面中得到的参数并将其连接到验证码的url上去
+    _url_captcha = _url_captcha + "?" + _url_captcha_tmp.split('?')[1]
+    print VIEWSTATE
+    print EVENTVALIDATION
+    print "待访问的带随机数字的验证码的url："
+    print _url_captcha
 
-# 先GET到`登录页面`
-_r0 = s.get(_url_loging, headers = headers0)
-# 解析GET到的页面中的两个关键元素
-# dict_post_params = parse_to_post_params(_r0.content)
-# print dict_post_params['__VIEWSTATE']
-# print dict_post_params['__EVENTVALIDATION']
-VIEWSTATE, EVENTVALIDATION, _url_captcha_tmp = parse_to_post_params(_r0.content)
-# 分割出从页面中得到的参数并将其连接到验证码的url上去
-_url_captcha = _url_captcha + "?" + _url_captcha_tmp.split('?')[1]
-print VIEWSTATE
-print EVENTVALIDATION
-print "待访问的带随机数字的验证码的url："
-print _url_captcha
+    # 从向这个url发出请求,然后将得到的图片保存到本地，最后解析图片成文字
+    _result_captcha = save_img_to_file_and_get_result(_url_captcha, _file_captcha)
 
-# 从向这个url发出请求,然后将得到的图片保存到本地
-_save_img_from_url(_url_captcha, _file_captcha)
-# 解析保存到本地的图片
-_result_captcha = _parse_img_to_txt(_file_captcha)
-print "验证码："
-print _result_captcha
+    print "验证码："
+    print _result_captcha
 
 
 
-# 将解析到的结果告诉url_payload
-_url_payload = '__EVENTTARGET=ctl00$contentParent$btLogin&__EVENTARGUMENT=&__VIEWSTATE='+ VIEWSTATE+ '&' +'__EVENTVALIDATION='+ EVENTVALIDATION + '&' +'ctl00$contentParent$UserName='+ _username + '&' +'ctl00$contentParent$PassWord='+ _password + '&' +'ctl00$contentParent$ValidateCode='+ _result_captcha
+    # 将解析到的结果告诉url_payload
+    _url_payload = '__EVENTTARGET=ctl00$contentParent$btLogin&__EVENTARGUMENT=&__VIEWSTATE='+ VIEWSTATE+ '&' +'__EVENTVALIDATION='+ EVENTVALIDATION + '&' +'ctl00$contentParent$UserName='+ _username + '&' +'ctl00$contentParent$PassWord='+ _password + '&' +'ctl00$contentParent$ValidateCode='+ _result_captcha
 
-_r1 = s.post(_url_relogin, data= _url_payload, headers=headers_post)
-print _r1.url
+    # 登录
+    _r1 = s.post(_url_relogin, data= _url_payload, headers=headers_post)
+    print _r1.url
 
+    # 课程查询
+    _r2 = s.get(_url_course_query, headers=headers_query)
+    print _r2.url
+    # 考试查询
+    _r3 = s.get(_url_exam_info, headers=headers_query)
+    print _r3.url
+    print _r3.content
 
-_r2 = s.get(_url_course_query, headers=headers_query)
-print _r2.url
-# TODO: 进行后续任意已登录的操作
+    # TODO: 进行后续任意已登录的操作
+
+if __name__ == "__main__":
+    main()
